db5d98335295a0162fca5d331e8da19fc2d959df
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-17 13:55:15 +0000

    second commit

--- project1/app.js
+++ project1/app.js
@@ -1,6 +1,15 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Becoming a Vue Ninja'
+    title: 'Becoming a Vue Ninja',
+    items: ['Mushroom', 'Green Shells', 'Red Shells', 'Banana', 'Star'],
+    ninjas: [
+      { name: 'Crystal', age: 25, belt: 'Black'},
+      { name: 'Ryu', age: 28, belt: 'Brown'},
+      { name: 'Ken', age: 30, belt: 'Blue'}
+    ]
+  },
+  methods: {
+  
   }
 })
